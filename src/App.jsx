154c963cc1201f2e22c105e139cb2ab4e01c5ab3
Dotims <dotims054@gmail.com>
2023-10-22 01:04:51 +0200

    introduction

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,17 @@ import ChatBot from './Components/ChatBot/ChatBot';
 import RecipeSearch from './Components/Food/RecipeSearch';
 import WikiSearch from './Components/Wikipedia/WikiSearch';
 import WeatherData from './Components/Weather/WeatherData';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Navbar />
       {/* <ChatBot />
       <RecipeSearch /> */}
-    {/* <WikiSearch /> */}
-    <WeatherData />
-    </div>
+      {/* <WikiSearch /> */}
+      {/* <WeatherData /> */}
+    </>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyledHeader } from './styles';
+
+const Navbar = () => {
+  return (
+    <StyledHeader>
+      <img src='' alt='' srcset='' />
+      <nav>
+        <ul>
+          <li>introduction</li>
+          <li>introduction</li>
+          <li>introduction</li>
+        </ul>
+      </nav>
+    </StyledHeader>
+  );
+};
+
+export default Navbar;
