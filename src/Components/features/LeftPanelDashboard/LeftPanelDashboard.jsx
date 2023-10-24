dbc6118ffd4b962a40c49958dc330dcea1c6f6a6
bezoski <rafalbuczkowski2004@wp.pl>
2023-10-24 18:35:54 +0200

    margin nie działa

--- src/Components/features/LeftPanelDashboard/LeftPanelDashboard.jsx
+++ src/Components/features/LeftPanelDashboard/LeftPanelDashboard.jsx
@@ -1,20 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import {} from "./styles";
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import {
+  StyledContainer,
+  StyledLogoContainer,
+  StyledLogo,
+  StyledNav,
+} from "./styles";
+import "./styles.css";
 
-const LeftPanelDashboard = () => {
-  return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/HomeDashboard">Home</Link>
-        </li>
-        <li>
-          <Link to="/SchoolDashboard">School</Link>
-        </li>
-      </ul>
-    </div>
-  );
-};
+class LeftPanelDashboard extends Component {
+  state = { clicked: false };
 
+  handleClick = () => {
+    const navbar = document.getElementById("navbar");
+    navbar.classList.toggle("active");
+    this.setState({ clicked: !this.state.clicked });
+  };
+
+  render() {
+    return (
+      <StyledContainer>
+        <StyledLogoContainer>
+          <StyledLogo>FlowVue</StyledLogo>
+        </StyledLogoContainer>
+        <ul
+          id="navbar"
+          className={this.state.clicked ? "nav_active" : "navbar"}
+        >
+          <li>
+            <NavLink
+              to="/HomeDashboard"
+              className={({ isActive }) => (isActive ? "link-active" : "link")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/SchoolDashboard"
+              className={({ isActive }) => (isActive ? "link-active" : "link")}
+            >
+              School
+            </NavLink>
+          </li>
+        </ul>
+      </StyledContainer>
+    );
+  }
+}
 export default LeftPanelDashboard;
--- src/Components/features/LeftPanelDashboard/styles.js
+++ src/Components/features/LeftPanelDashboard/styles.js
@@ -1,6 +1,19 @@
 import { styled } from "@mui/material/styles";
 import globalTheme from "../GlobalStyles/GlobalTheme";
 
-// export const StyledText = styled("p")(({ theme }) => ({
-//   color: theme.palette.primary.main,
-// }));
+export const StyledContainer = styled("div")(({ theme }) => ({
+  height: "900px",
+  width: "270px",
+  background: globalTheme.palette.primary.main,
+  borderRadius: "0 35px 35px 0",
+}));
+
+export const StyledLogoContainer = styled("div")(({ theme }) => ({
+  background: globalTheme.palette.primary.dark,
+  marginTop: "40px",
+  height: "60px",
+}));
+
+export const StyledLogo = styled("nav")(({ theme }) => ({}));
+
+export const StyledNav = styled("nav")(({ theme }) => ({}));
