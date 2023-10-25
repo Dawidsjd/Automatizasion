02177c8a44a826d926c6563249d7dd2b72377c81
atek <53567837+Dawidsjd@users.noreply.github.com>
2023-10-25 19:33:19 +0200

    reparing bugs related to bot

--- src/Components/features/ChatBot/ChatBot.js
+++ src/Components/features/ChatBot/ChatBot.js
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import styles from './style';
 import ChatIcon from '@mui/icons-material/Chat';
 import SendIcon from '@mui/icons-material/Send';
+import BotAvatar from '../../../assets/200.gif';
+import UserAvatar from '../../../assets/gagajhagha.jpg';
 
 function ChatBot() {
   const [messages, setMessages] = useState([]);
   const [query, setQuery] = useState('');
-  const [isChatOpen, setIsChatOpen] = useState(false); // Dodajemy nowy stan
+  const [isChatOpen, setIsChatOpen] = useState(false); 
+  const [isBotTyping, setIsBotTyping] = useState(false);
 
   const addMessage = (content, role) => {
     setMessages(prevMessages => [...prevMessages, { content, role }]);
@@ -14,12 +17,15 @@ function ChatBot() {
 
   const handleUserMessage = async (e) => {
     e.preventDefault();
-    addMessage(query, 'user');
-    await fetchData(query);
-    setQuery('');
+    if (query.trim() !== '') {
+      setQuery('');
+      addMessage(query, 'user');
+      await fetchData(query);
+    }
   };
 
   const fetchData = async (userMessage) => {
+    setIsBotTyping(true);
     const url = 'https://lemurbot.p.rapidapi.com/chat';
     const options = {
       method: 'POST',
@@ -38,49 +44,99 @@ function ChatBot() {
     try {
       const response = await fetch(url, options);
       const result = await response.json();
+      setIsBotTyping(false);
       addMessage(result.data.conversation.output, 'bot');
     } catch (error) {
+      setIsBotTyping(false);
       console.error(error);
     }
   };
 
   const toggleChat = () => {
-    setIsChatOpen(!isChatOpen); // Zmieniamy stan po kliknięciu
+    setIsChatOpen(!isChatOpen);
   };
 
   return (
     <div style={styles.container}>
-      
       {isChatOpen && (
         <div style={styles.ChatContainer}>
-        <div style={styles.Banner}>ChatBot</div>
-        <div style={styles.MessagesContainer}>
-          {messages.map((message, index) => (
-            <div key={index} style={{
-              marginBottom: '10px',
-              fontSize: "14px",
-              alignSelf: message.role === 'user' ? 'flex-end' : 'flex-start',
-              backgroundColor: message.role === 'user' ? 'purple' : '#f0f0f0',
-              color: message.role === 'user' ? '#fff' : 'inherit', // Ustawiamy kolor tekstu tylko dla wiadomości od użytkownika
-              padding: '5px',
-              borderRadius: '5px'
-            }}>
-              {message.role === 'user' ? 'You: ' : 'Bot: '}
-              {message.content}
-            </div>
-          ))}
+          <div style={styles.Banner}>ChatBot</div>
+          <div style={styles.MessagesContainer}>
+            {messages.map((message, index) => (
+              <div
+                key={index}
+                style={{
+                  marginBottom: '10px',
+                  fontSize: "14px",
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: message.role === 'user' ? 'flex-end' : 'flex-start',
+                }}
+              >
+                {message.role === 'user' ? (
+                  <>
+                    <div
+                      style={{
+                        backgroundColor: 'purple',
+                        color: '#fff',
+                        padding: '5px 10px',
+                        borderRadius: '5px',
+                        maxWidth: '60%',
+                      }}
+                    >
+                      {message.content}
+                    </div>
+                    <img
+                      src={UserAvatar}
+                      alt="User Avatar"
+                      style={{
+                        width: '30px',
+                        height: '30px',
+                        borderRadius: '50%',
+                        marginLeft: '10px',
+                      }}
+                    />
+                  </>
+                ) : (
+                  <>
+                    {isBotTyping && <div>Bot is typing...</div>}
+                    <img
+                      src={BotAvatar}
+                      alt="Bot Avatar"
+                      style={{
+                        width: '30px',
+                        height: '30px',
+                        borderRadius: '50%',
+                        marginRight: '10px',
+                      }}
+                    />
+                    <div
+                      style={{
+                        backgroundColor: '#f0f0f0',
+                        padding: '5px 10px',
+                        borderRadius: '5px',
+                        maxWidth: '60%',
+                      }}
+                    >
+                      {message.content}
+                    </div>
+                  </>
+                )}
+              </div>
+            ))}
+          </div>
+          <form style={styles.SendContainer} onSubmit={handleUserMessage}>
+            <input
+              type="text"
+              style={styles.MsgSend}
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              placeholder="Enter your message"
+              disabled={messages.length > 0 && messages[messages.length - 1].role === 'user'}
+            />
+            <button style={styles.MsgSendBtn} type="submit"><SendIcon /></button>
+          </form>
         </div>
-        <form style={styles.SendContainer} onSubmit={handleUserMessage}>
-        <input 
-          type="text" 
-          style={styles.MsgSend}
-          value={query} 
-          onChange={(e) => setQuery(e.target.value)} 
-          placeholder="Enter your message" 
-        />
-        <button style={styles.MsgSendBtn} type="submit"><SendIcon /></button>
-      </form>
-      </div>
       )}
       <button style={styles.toggleButton} onClick={toggleChat}><ChatIcon style={styles.chatIcon}/></button>
     </div>
