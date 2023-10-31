5561a2cd2edd5aeab771351fcbc8c2559908703b
Michaluszeq <95742362+Michaluszeq@users.noreply.github.com>
2023-10-31 15:32:26 +0100

    Connect buttona to SchooolDashboard

--- src/Components/Note/Note.jsx
+++ src/Components/Note/Note.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import {} from "./styles";
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 import { Link } from "react-router-dom";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { styles } from "./styles"; // Importuj style
 
 function Notepad() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
 
   return (
-    <div className="notepad">
+    <div style={styles.notepad}>
       <div className="notepad-header">
         <input
           type="text"
@@ -24,8 +24,14 @@ function Notepad() {
           onChange={(e) => setContent(e.target.value)}
         ></textarea>
       </div>
-      <div className="notepad-footer">
+      <div className="saveButton">
         <button onClick={() => saveNote()}>Save</button>
+        
+        <Link to="/SchoolDashboard">
+          <button style={styles.exitBtn}>
+            <KeyboardBackspaceIcon />
+          </button>
+        </Link>
       </div>
     </div>
   );
