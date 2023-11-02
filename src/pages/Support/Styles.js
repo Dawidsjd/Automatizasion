2d9fda212c66f09887f2bd8cbcd6a81b141c9b5c
Dotims <dotims054@gmail.com>
2023-11-02 21:48:34 +0100

    changes support subpage

--- src/pages/Support/Styles.js
+++ src/pages/Support/Styles.js
@@ -1,17 +1,35 @@
 import { styled } from '@mui/material/styles';
 
 export const StyledSupportContainer = styled('main')(({ theme }) => ({
+  background: '#dfe6e9',
   display: 'flex',
+  justifyContent: 'center',
 }));
 
 export const StyledWrapper = styled('div')(({ theme }) => ({
+  background: 'white',
+  borderRadius: '25px',
+  margin: '200px 0',
+  paddingTop: '50px',
   width: '100%',
-  height: '100vh',
   display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
+  [theme.breakpoints.up('lg')]: {
+    width: '80%',
+    // margin: '200px 0',
+  },
 }));
 
 export const StyledMain = styled('div')(({ theme }) => ({
   width: '100%',
+}));
+
+export const StyledWrapperContent = styled('div')(({ theme }) => ({
+  width: '80%',
+  textAlign: 'center',
+  h1: {
+    fontSize: '46px',
+  },
 }));
--- src/pages/Support/Support.jsx
+++ src/pages/Support/Support.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 import { InlineWidget } from 'react-calendly';
 import Header from '../../Components/HomeAssets/Navbar/Navbar';
-import { StyledSupportContainer, StyledMain, StyledWrapper } from './Styles';
+import {
+  StyledSupportContainer,
+  StyledMain,
+  StyledWrapper,
+  StyledWrapperContent,
+  StyledInlineWidget,
+} from './Styles';
+import Footer from '../../Components/HomeAssets/Footer/Footer';
 
 const Support = () => {
   return (
-    <StyledSupportContainer>
+    <>
       <Header />
-
-      <StyledWrapper>
-        <StyledMain>
-          <InlineWidget url='https://calendly.com/flowview/contact' />
-        </StyledMain>
-      </StyledWrapper>
-    </StyledSupportContainer>
+      <StyledSupportContainer>
+        <StyledWrapper>
+          <StyledWrapperContent>
+            <h1>Contact</h1>
+            <p>
+              We invite you to engage in a complimentary consultation where we
+              can discuss the topic you've brought to our attention. Our team is
+              here to assist you, and we look forward to a fruitful
+              conversation. Please feel free to reach out, and we will be more
+              than happy to help. Thank you! 😊
+            </p>
+          </StyledWrapperContent>
+          <StyledMain>
+            <InlineWidget url='https://calendly.com/flowview/contact' />
+          </StyledMain>
+        </StyledWrapper>
+      </StyledSupportContainer>
+      <Footer />
+    </>
   );
 };
 
