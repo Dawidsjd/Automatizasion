c968866d89182aa86a7924bde469fb645d5864a3
Michaluszeq <95742362+Michaluszeq@users.noreply.github.com>
2023-11-02 23:30:27 +0100

    zmiany w notatniku (style i działanie)

--- src/Components/Note/Note.jsx
+++ src/Components/Note/Note.jsx
@@ -1,48 +1,104 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-import { styles } from "./styles"; // Poprawiona ścieżka do importu
+import { styles } from "./styles"; // Import pliku ze stylami
+import { Container } from "@mui/material";
 
-function Notepad() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+function App() {
+  
+  
+  const [notes, setNotes] = useState([]);
+  const [state, setState] = useState({
+    title: "",
+    note: "",
+    id: Math.random() * 10,
+  });
+
+  const handleDelete = (id) => {
+    const leftNotes = notes.filter((note) => note.id !== id);
+    setNotes(leftNotes);
+  };
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setNotes([...notes, state]);
+    setState({
+      title: "",
+      note: "",
+    });
+  };
 
   return (
-    <div style={styles.notepad}>
-      <div style={styles.Background}>
-      <h1 style={styles.title}>Note</h1>
-      <div className="notepad-header" style={styles.notepadHeader}>
-        <input
-          type="text"
-          placeholder="Note Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+    <div style={styles.background}>
+      <h1 style={styles.h1}>Notes</h1>
+      <div style={styles.addNote}>
+        <form style={styles.form} onSubmit={handleSubmit}>
+          <input style={styles.input}
+            type="text"
+            placeholder="title"
+            name="title"
+            onChange={handleChange}
+            value={state.title}
+            required
+          />
+          <textarea
+            style={styles.note}
+            name="note"
+            id=""
+            cols="30"
+            rows="5"
+            placeholder="note"
+            className="border-2 border-blue-200 p-2"
+            onChange={handleChange}
+            value={state.note}
+            required
+          ></textarea>
+          <button
+            type="subit"
+            style={styles.button}
+          >
+            Add Note
+          </button>
+        </form>
       </div>
-      <div className="notepad-content" style={styles.notepadContent}>
-        <textarea
-          placeholder="Note Content"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
-          style={styles.textarea}
-        ></textarea>
-      </div>
-      <div className="saveButton" style={styles.saveButton}>
-        <button onClick={() => saveNote()}>Save</button>
-      </div>
-      <Link to="/SchoolDashboard">
+
+      <div style={styles.notes_container}>
+        {
+          notes.length > 0 ? notes.map((note, i) => {
+            return (
+              <div
+              style={styles.notes}
+                key={i}
+              >
+                <button
+                style={styles.deleteButton}
+                  onClick={() => handleDelete(note.id)}
+                >
+                  x
+                </button>
+                <h3 style={styles.noteTitle}>{note.title}</h3>
+                <p>{note.note}</p>
+              </div>
+              
+            );
+          }) : <p style={styles.p}>
+            No notes available.
+          </p>
+        }
+        <Link to="/SchoolDashboard">
         <button style={styles.exitBtn}>
           <KeyboardBackspaceIcon />
         </button>
       </Link>
+      </div>
     </div>
-    </div>
+    
   );
-
-  function saveNote() {
-    //tu podłączenie do bazy
-    console.log("Zapisano notatkę:", title, content);
-  }
 }
 
-export default Notepad;
+export default App;
--- src/Components/Note/styles.js
+++ src/Components/Note/styles.js
@@ -1,47 +1,90 @@
+import ContentBackground from "../../assets/content.svg";
 import globalTheme from "../features/GlobalStyles/GlobalTheme";
-
 export const styles = {
-  notepad: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100vh",
-    backgroundColor: "#88a2c4",
+  background: {
+    backgroundColor: '#a9c9dd',
+    width: '100%',
+    minHeight: '100vh',
+    position: 'absolute',
+    zIndex: -1,
   },
-  Background: {
-    background: "#a9c9dd",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    width: "1100px",
-    height: "700px",
-    textAlign: "center",
-    margin: "128px auto",
-    borderRadius: "10px",
-    paddingBottom: "32px",
-    overflowY: "auto",
-    boxShadow: "0 0 10px rgba(0, 0, 0, 0.3)", // Dodaj box-shadow
+  h1: {
+    textAlign: 'center',
+    fontSize: '2.5rem',
+    padding: '1.25rem',
   },
-
-  notepadHeader: {
-    width: "100%",
-    padding: "1rem",
-    textAlign: "left",
-    marginLeft: "30px",
+  addNote: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  notepadContent: {
-    width: "100%",
-    padding: "1rem",
-    textAlign: "center",
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
   },
-  saveButton: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+  input: {
+    borderWidth: '2px',
+    borderColor: '#4299e1',
+    padding: '0.5rem',
+    marginBottom: '0.5rem',
   },
-  exitBtn: {
+  button: {
+    backgroundColor: '#4299e1',
+    padding: '1.25rem 2rem',
+    color: 'white',
+    background: globalTheme.palette.primary.main,
+    marginTop: '1rem',
+    size: '90px',
+  },
+  notes_container: {
+    borderTopWidth: '2px',
+    borderTopColor: '#4299e1',
+    margin: '2.5rem',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  notes: {
+    backgroundColor: '#F7EC9A',
+    marginTop: '1.25rem',
+    marginRight: '1.25rem',
+    width: '300px',
+    padding: '1rem',
+    paddingTop: '2.5rem',
+    position: 'relative',
+    overflow: 'auto',
+    fontSize: '16px',
+    wordWrap: 'break-word',
+    whiteSpace: 'pre-line',
+  },
+  deleteButton: {
+    position: 'absolute',
+  right: '0px',
+  top: '0',
+  fontWeight: 'bold',
+  fontSize: '1.5rem',
+  color: 'red',
+  backgroundColor: 'transparent', // Tło przycisku jest teraz przeźroczyste
+  border: 'none', // Usuwamy obramowanie przycisku, jeśli istnieje
+  },
+  noteTitle: {
+    fontWeight: 'bold',
+    fontSize: '1rem',
+    paddingBottom: '0.5rem',
+  },
+  p: {
+    paddingTop: '1.25rem',
+    paddingBottom: '1.25rem',
+  },
+  // Przykład responsywności
+  '@media (max-width: 768px)': {
+    h1: {
+      fontSize: '2rem',
+    notes: {
+      width: '100%',
+    },
+    }.fontSize
+  },
+   exitBtn: {
     position: "absolute",
     top: "10px",
     left: "10px",
@@ -54,8 +97,5 @@ export const styles = {
     alignItems: "center",
     fontSize: "1.5rem",
     padding: "10px 30px",
-  },
-  input: {
-    border: "2px solid black", // Dodaj to pole, aby dodać czarne obramowanie
   },
 };
