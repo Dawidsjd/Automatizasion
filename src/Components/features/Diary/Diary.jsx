e7be093947b4b1a1c25c1e0cb345b29f824e7f58
atek <53567837+Dawidsjd@users.noreply.github.com>
2023-11-02 18:28:43 +0100

    next steps in Diary

--- src/Components/features/Diary/Diary.jsx
+++ src/Components/features/Diary/Diary.jsx
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import {
+  Container,
+  FormContainer,
+  SubmitButton,
+  SliderContainer,
+} from './styles';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 
 const Diary = () => {
   const [indexNumber, setIndexNumber] = useState('');
   const [schoolSymbol, setSchoolSymbol] = useState('');
   const [password, setPassword] = useState('');
   const [lessons, setLessons] = useState([]);
+  const [formVisible, setFormVisible] = useState(true);
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
@@ -22,13 +39,13 @@ const Diary = () => {
       console.log("nauczyciel: ", response.data[0].teacherPrimary.displayName)
       console.log("czas: ", response.data[0].timeSlot.display)
 
-      setLessons(response.data); // Zapisz lekcje w stanie komponentu
+      setLessons(response.data);
+      setFormVisible(false);
     } catch (error) {
       console.error('Error logging in:', error);
     }
   };
 
-  // Funkcja do grupowania lekcji według daty
   const groupLessonsByDate = (lessons) => {
     const groupedLessons = {};
     lessons.forEach(lesson => {
@@ -43,68 +60,87 @@ const Diary = () => {
 
   const groupedLessons = groupLessonsByDate(lessons);
 
-  return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-      <div>
-        <label>
-          Index Number:
-          <input
-            type="text"
-            value={indexNumber}
-            onChange={(e) => setIndexNumber(e.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          School Symbol:
-          <input
-            type="text"
-            value={schoolSymbol}
-            onChange={(e) => setSchoolSymbol(e.target.value)}
-          />
-        </label>
-      </div>
-      <div>
-        <label>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+  const sliderSettings = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
 
-      <div>
-        {Object.keys(groupedLessons).map((date, index) => (
-          <div key={index}>
-            <h2>{date}</h2>
-            <table>
-              <thead>
-                <tr>
-                  <th>Subject</th>
-                  <th>Room</th>
-                  <th>Teacher</th>
-                  <th>Time</th>
-                </tr>
-              </thead>
-              <tbody>
-                {groupedLessons[date].map((lesson, index) => (
-                  <tr key={index}>
-                    <td>{lesson.subject.name}</td>
-                    <td>{lesson.room.code}</td>
-                    <td>{lesson.teacherPrimary.displayName}</td>
-                    <td>{lesson.timeSlot.display}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ))}
+  return (
+    <div style={Container}>
+      {formVisible && (
+        <div style={FormContainer}>
+          <form onSubmit={handleFormSubmit}>
+            <div>
+              <label>
+                Index Number:
+                <input
+                  type="text"
+                  value={indexNumber}
+                  onChange={(e) => setIndexNumber(e.target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                School Symbol:
+                <input
+                  type="text"
+                  value={schoolSymbol}
+                  onChange={(e) => setSchoolSymbol(e.target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Password:
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </label>
+            </div>
+            <button type="submit" style={SubmitButton}>Submit</button>
+          </form>
+        </div>
+      )}
+
+      <div style={SliderContainer}>
+        <Slider {...sliderSettings}>
+          {Object.keys(groupedLessons).map((date, index) => (
+            <div key={index} style={{ minWidth: '300px', margin: '10px' }}>
+              <h2>{date}</h2>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Subject</TableCell>
+                      <TableCell align="right">Room</TableCell>
+                      <TableCell align="right">Teacher</TableCell>
+                      <TableCell align="right">Time</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {groupedLessons[date].map((lesson, index) => (
+                      <TableRow
+                        key={index}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                      >
+                        <TableCell>{lesson.subject.name}</TableCell>
+                        <TableCell align="right">{lesson.room.code}</TableCell>
+                        <TableCell align="right">{lesson.teacherPrimary.displayName}</TableCell>
+                        <TableCell align="right">{lesson.timeSlot.display}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
---
+++ src/Components/features/Diary/styles.js
@@ -0,0 +1,31 @@
+import ContentBackground from '../../../assets/content.svg';
+
+export const Container = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    background: `#677bab url(${ContentBackground}) no-repeat center center fixed`,
+  };
+  
+  export const FormContainer = {
+    marginBottom: '20px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh'
+  };
+  
+  export const TableContainer = {
+    maxWidth: '800px',
+    marginBottom: '50px',
+  };
+  
+  export const SubmitButton = {
+    marginTop: '10px',
+  };
+  
+  export const SliderContainer = {
+    maxWidth: '800px',
+  };
+  
+  
