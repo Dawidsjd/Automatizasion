0ee7747a7a899efd31279c48a7dc5f182dc76a8e
atek <53567837+Dawidsjd@users.noreply.github.com>
2023-11-06 17:23:05 +0100

    changing minScale on SchoolModel

--- src/Components/Model3D/School3D.js
+++ src/Components/Model3D/School3D.js
@@ -27,7 +27,7 @@ const ThreeScene = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={0}
           maxDistance={10}
-          minDistance={4}
+          minDistance={4.5}
         />
 
         <Environment preset='forest' />
--- src/Components/features/Diary/Diary.jsx
+++ src/Components/features/Diary/Diary.jsx
@@ -41,7 +41,7 @@ const Diary = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:5000/api/login", {
+      const response = await axios.post("https://automatizasion.onrender.com/api/login", {
         indexNumber,
         schoolSymbol,
         password,
